a45bf47f1d40089193a90ef4cd8fe4239b698b67
bumfresh <bumbum@cooperativa.cat>
2020-07-11 07:04:59 +0200

    base

--- config/settings/base.py
+++ config/settings/base.py
@@ -101,9 +101,17 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ndmadb',
+        'USER': 'ndma',
+        'PASSWORD': 'gambiaconnection',
+        'HOST': 'postgres://sukggzgrxgtual:52d84de485cbb2e5642af5f200cfead1e592a2e8b825570c03b1a9d3707e8dc5@ec2-50-17-90-177.compute-1.amazonaws.com:5432/dakq01t96ige37',
+        'PORT': '',
     }
 }
 
